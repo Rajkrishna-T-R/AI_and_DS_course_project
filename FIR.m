ce89c902b071aa92f7a7e86c415a37329c7b2545
Rajkrishna-T-R <rajkrishna.thevalil@gmail.com>
2025-09-12 00:53:08 +0530

    Create FIR.m

---
+++ FIR.m
@@ -0,0 +1,94 @@
+function [filtered_data_final] = FIR(path,high_cutoff,low_cutoff)
+
+
+% This function returns the same array as [2 x 100]
+%  each with [64 x 512] the original data
+  
+arguments (Input)
+ path
+ high_cutoff
+ low_cutoff
+end
+
+arguments (Output)
+  filtered_data_final
+end
+
+data=load(path);
+
+all_trials = data.eeg_data_wrt_task_rep_no_eog_256Hz_end_trial;
+
+
+fs = 256;              % Sampling frequency (Hz)  
+order = 100;           % Filter order (higher = sharper) 
+num_trials=100;
+num_channels=64;
+num_samples=512;
+
+
+ % More commonly used order is higher order ex:100 for EEG 
+
+
+% filter design
+
+Wn = [low_cutoff high_cutoff] / (fs/2); % Bandpass low to up Hz, normalized
+
+b = fir1(order, Wn, 'bandpass');        % FIR filter coefficients
+
+
+
+
+
+filtered_data_final =cell(2,num_trials); % 2 x 100
+
+% each cell is  [64 x 512]
+
+
+
+
+% Filtering Each channel in one trail--
+
+% ----- class 1 -----
+
+
+for trial_num = 1:num_trials
+
+    trial = all_trials{1, trial_num}; % channel x samples
+
+    filtered_trial=zeros(num_channels,num_samples);
+
+    for ch=1:num_channels
+
+        filtered_trial(ch,:) = filter(b,1,trial(ch,:)); 
+
+            % each trail is 64 * 512 as given in the data set
+                                                  
+    end
+  filtered_data_final{1,trial_num} = filtered_trial;
+end
+
+
+% ----- class 2 -----
+
+
+
+
+for trial_num = 1:num_trials
+
+    trial = all_trials{2, trial_num}; % channel x samples
+    filtered_trial=zeros(num_channels,num_samples);
+
+    for ch=1:num_channels
+
+        filtered_trial(ch,:) = filter(b,1,trial(ch,:)); 
+
+            % each trail is 64 * 512 as given in the data set
+                                                  
+    end
+  filtered_data_final{2,trial_num}=filtered_trial;
+end
+
+disp(size(filtered_data_final));
+
+
+end
